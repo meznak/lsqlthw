027af403fe737df61ad5f038c23eee9210bd6515
Nathan Plamondon <nplamondon@gmail.com>
2014-11-26 10:21:45 -0700

    fixed ex3 typo

--- ex3.sql
+++ ex3.sql
@@ -4,4 +4,4 @@ INSERT INTO person (id, first_name, last_name, age)
 INSERT INTO pet (id, name, breed, age, dead)
   VALUES (0, "Fluffy", "Unicorn", 1000, 0);
 
-INSERT INTO pet VALUES (1, "Gicantor", "Robot", 1, 1);
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
