da9b14de9c261e615ac8be37c7de6c0a8afaf0ce
Nathan Plamondon <nplamondon@gmail.com>
2014-11-26 11:08:41 -0700

    ex11

---
+++ ex11.sql
@@ -0,0 +1,13 @@
+/* Should fail because 0 is already taken */
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Frank', 'Smith', 100);
+
+INSERT OR REPLACE INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Frank', 'Smith', 100);
+
+SELECT * FROM person;
+
+REPLACE INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Zed', 'Shaw', 37);
+
+SELECT * FROM person;
